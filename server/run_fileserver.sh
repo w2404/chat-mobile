1abcf3376d9718c9d93f66a5b53ff65fd14afa3d
w2404 <wanke2404@163.com>
2023-03-23 18:50:20 +0800

    sudo

--- server/run_fileserver.sh
+++ server/run_fileserver.sh
@@ -1,2 +1,3 @@
 cd ../html
-nohup sudo python -m http.server 8443 > ../log.txt 2>&1 &
+#nohup sudo python -m http.server 8443 > ../log.txt 2>&1 &
+nohup python -m http.server 8443 > ../log.txt 2>&1 &
--- server/run_remoteserver.sh
+++ server/run_remoteserver.sh
@@ -1,1 +1,2 @@
-nohup sudo python ./remoteserver.py 8444 > ../log.txt 2>&1 &
+#nohup sudo python ./remoteserver.py 8444 > ../log.txt 2>&1 &
+nohup python ./remoteserver.py 8444 > ../log.txt 2>&1 &
