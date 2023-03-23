9a647ffd2e7115df7d9aab907f63808888a4de77
w2404 <wanke2404@163.com>
2023-03-23 17:37:06 +0800

    server

--- server/fileserver.py
+++ server/fileserver.py
@@ -3,6 +3,6 @@ import sys
 import ssl
 p=sys.argv[1] #证书路径
 
-httpd = http.server.HTTPServer(('0.0.0.0', 4443), http.server.SimpleHTTPRequestHandler)
+httpd = http.server.HTTPServer(('0.0.0.0', 443), http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket, certfile=p, server_side=True)
 httpd.serve_forever()
